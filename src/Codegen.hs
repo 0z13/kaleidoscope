29e937b9cffc5272f8b2c6657364f615e3170be7
Joe <zielinski@fastmail.com>
2021-04-28 14:20:10 +0200

    Pretty confused

--- src/Codegen.hs
+++ src/Codegen.hs
@@ -1,3 +1,30 @@
 module Codegen where
 
+import LLVM.AST
+import LLVM.AST.Global
+import qualified LLVM.AST as AST
+import Data.Map as M
+import qualified LLVM.AST.Linkage as L
+import qualified LLVM.AST.Constant as C
+import qualified LLVM.AST.Attribute as A
+import qualified LLVM.AST.CallingConvention as CC
+import qualified LLVM.AST.FloatingPointPredicate as FP
 -- And we sang, "Here we go again". 
+-- Yes, i love james blunt
+
+-- A IEEE magic number
+double :: Type
+double = FloatingPointType DoubleFP 
+
+-- Pretty confused.
+
+type SymbolTable = [(String, Operand)]
+
+data CodegenState = CodeGenState {
+  currentBlock :: Name
+, blocks       :: Map.Map 
+, symtab       :: SymbolTable
+, blockCount   :: It
+, count        :: Word
+, names        :: Names 
+} 
